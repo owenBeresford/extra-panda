69fde5b9643a771308daa2b2d8127bb81c26778e
oab1 <oab1@users.sourceforge.net>
2024-07-30 23:56:07 +0100

    Add eslint rules for jsdoc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import jsdoc from 'eslint-plugin-jsdoc';
 
 export default [
   { languageOptions: { globals: globals.browser }},
@@ -12,20 +13,22 @@ export default [
     jsdoc: {
       mode: "typescript",
     },
-    plugins: { "jsdoc"
-    },
+	},
+    plugins: { jsdoc },
 	"rules": { 
 	"complexity": ["error", 10],	
     "jsdoc/check-tag-names": 1,
-    "jsdoc/newline-after-description": "off",
+    "jsdoc/require-jsdoc": 1,
+    "jsdoc/newline-after-description": 0,
     "jsdoc/require-description": 1,
-    "jsdoc/require-param": [1, { checkDestructured: false }],
-    "jsdoc/require-param-description": 1,
+//    "jsdoc/require-param": [1, { checkDestructured: false }],
+    "jsdoc/require-param": 1,
+    "jsdoc/require-param-description": 0,
     "jsdoc/require-param-name": 1,
     "jsdoc/require-param-type": 1,
     "jsdoc/require-returns": 1,
-    "jsdoc/require-returns-description": 1,
+    "jsdoc/require-returns-description": 0,
     "jsdoc/require-yields": 1,
 		},
- }
+  }
 ];
