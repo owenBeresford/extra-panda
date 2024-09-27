e1d24fde539f9954cfdd9cf4ef2604424f9fbda1
oab1 <oab1@users.sourceforge.net>
2024-09-27 20:39:17 +0100

    Add util to ensure required data is present (bash as faster)

--- tools/checksum.bash
+++ tools/checksum.bash
@@ -19,8 +19,17 @@ if [ ! -f "$BASE/package-lock.json" ];then
 	echo "Did you run 'npm install'?  There are missing files."
 	exit 2
 fi
+if [ -z "`which chromium`" ]; then
+	echo "Pls install chromium, or reconfig this test for other browsers that support the debug protocol (see playwright docs)"
+	exit 3
+fi
+if [ -n "`ps axw | grep ignore-this | grep -v grep`" ]; then
+	echo "WARNING a test instance of Chromium is still running.  Aborting this checksum script"
+	exit 4
+fi
 
-if [ ! -f "$BASE/dist/tests" ]; then
+
+if [ ! -d "$BASE/dist/tests" ]; then
 	mkdir -p $BASE/dist/tests
 fi
 if [ -z  `ls "$BASE/dist/tests/*.mjs"` ]; then
@@ -28,10 +37,14 @@ if [ -z  `ls "$BASE/dist/tests/*.mjs"` ]; then
 fi
 
 # https://www.golinuxcloud.com/openssl-cheatsheet/
+# if you are enterprise, please set more restrictions to access.
 if [ ! -f "$BASE/src/fixtures/*.pem" ];then
-	openssl ecparam -name prime256v1 -genkey -out "$BASE/src/fixtures/ecdsa_private.key"
-	openssl req -new -x509 -days 365 -key "$BASE/src/fixtures/ecdsa_private.key" -out "$BASE/src/fixtures/ecdsa.crt"
-	openssl ec -in "$BASE/src/fixtures/ecdsa_private.key" -pubout -out "$BASE/src/fixtures/ecdsa.pem" 
+#	openssl ecparam -name prime256v1 -genkey -out "$BASE/src/fixtures/ecdsa_private.key"
+#	openssl req -new -x509 -days 365 -key "$BASE/src/fixtures/ecdsa_private.key" -out "$BASE/src/fixtures/ecdsa.crt"
+#	openssl ec -in "$BASE/src/fixtures/ecdsa_private.key" -pubout -out "$BASE/src/fixtures/ecdsa.pem" 
+	openssl genrsa -out "$BASE/src/fixtures/private.key"
+	openssl req -new -key "$BASE/src/fixtures/private.key" -out "$BASE/src/fixtures/csr.pem"
+	openssl x509 -req -days 365 -in "$BASE/src/fixtures/csr.pem" -signkey "$BASE/src/fixtures/private.key" -out "$BASE/src/fixtures/cert.pem"
 fi
 	
 # vim: nospell syn=bash
