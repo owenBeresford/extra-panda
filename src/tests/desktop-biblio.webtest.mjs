de1b7fa1392829be4a33d0f4c18a9bb215c2f52e
oab1 <oab1@users.sourceforge.net>
2024-11-10 12:48:49 +0000

    New impl for applyDOMposition

--- src/tests/desktop-biblio.webtest.mjs
+++ src/tests/desktop-biblio.webtest.mjs
@@ -21,7 +21,7 @@ describe("TEST BROWSER desktop-biblio", async () => {
     throw new Error("This is a browser only test");
   }
 
-  it("go 6: applyDOMpositions", async () => {
+  it("test 6: applyDOMpositions", async () => {
     const TEST_NAME = "BROWSER TEST func[1] applyDOMPositions";
     // IOIO add extra test, where the base HTML is adjusted to the test results mean more
     return await wrap(
@@ -41,17 +41,87 @@ describe("TEST BROWSER desktop-biblio", async () => {
         applyDOMpositions(dom.querySelector("#uniq3"), win);
         applyDOMpositions(dom.querySelector("#uniq4"), win);
         expect(dom.querySelector("#uniq1").getAttribute("class")).toBe(
-          "leanIn leanUp",
+          "leanLeft leanUp",
         ); //  "assert #19",
         expect(dom.querySelector("#uniq2").getAttribute("class")).toBe(
-          "leanIn leanUp",
+          "leanLeft leanUp",
         ); //  "assert #20",
         expect(dom.querySelector("#uniq3").getAttribute("class")).toBe(
-          "leanIn leanUp",
+          "leanLeft leanUp",
         ); // "assert #21",
         expect(dom.querySelector("#uniq4").getAttribute("class")).toBe(
-          "leanIn leanUp",
+          "leanLeft leanUp",
         ); // "assert #22",
+        await delay(100);
+      },
+    );
+  });
+
+  it("test 6.1: applyDOMpositions", async () => {
+    const TEST_NAME = "BROWSER TEST func[2] applyDOMPositions";
+    // IOIO add extra test, where the base HTML is adjusted to the test results mean more
+    return await wrap(
+      TEST_NAME,
+      "https://127.0.0.1:8081/domposition.html",
+      async (dom, loc, win) => {
+        // currently too high and left
+        applyDOMpositions(
+          dom.querySelector(".addReferences sup:nth-child(3)>a"),
+          win,
+        );
+        // currently too left by alot
+        applyDOMpositions(
+          dom.querySelector(
+            '.addReferences sup>a[href="https://www.highcharts.com/demo/heatmap-canvas"]',
+          ),
+          win,
+        );
+        // currently too left
+        applyDOMpositions(
+          dom.querySelector(
+            '.addReferences sup>a[href="https://www.cybrosys.com/blog/create-dashboard-using-highchart-js"]',
+          ),
+          win,
+        );
+        // currently OK
+        applyDOMpositions(
+          dom.querySelector(
+            '.addReferences sup>a[href="https://api.highcharts.com/highcharts/chart.events.render"]',
+          ),
+
+          win,
+        );
+
+        expect(
+          dom
+            .querySelector(".addReferences sup:nth-child(3)>a")
+            .getAttribute("class"),
+        ).toBe("leanCentre");
+        expect(
+          dom // labelled 9
+            .querySelector(
+              '.addReferences sup>a[href="https://www.highcharts.com/demo/heatmap-canvas"]',
+            )
+            .getAttribute("class"),
+        ).toBe("leanLeft");
+        expect(
+          dom
+            .querySelector(
+              '.addReferences sup>a[href="https://www.cybrosys.com/blog/create-dashboard-using-highchart-js"]',
+            )
+            .getAttribute("class"),
+        ).toBe("leanCentre");
+
+        expect(
+          dom
+            .querySelector(
+              '.addReferences sup>a[href="https://api.highcharts.com/highcharts/chart.events.render"]',
+            )
+            .getAttribute("class"),
+        ).toBe("leanRight");
+
+        // need a further test for centre
+
         await delay(100);
       },
     );
