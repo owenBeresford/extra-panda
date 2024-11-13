332eff3a35bdb073fe60cf18d30ca671e93a70f0
oab1 <oab1@users.sourceforge.net>
2024-11-13 20:27:01 +0000

    Make type sniffing tests work by changing file type

---
+++ src/tests/cookie.vitest.ts
@@ -0,0 +1,35 @@
+import { describe, it, assertType } from "vitest";
+
+import { page } from "./page-seed-vite";
+import { Cookieable } from "../all-types";
+import { isFullstack } from "../dom-base";
+import { TEST_ONLY } from "../cookies";
+
+const { QOOKIE, } = TEST_ONLY;
+
+describe("TEST cookies", () => {
+  it("go 1: getCookie ", () => {
+    assertType < Cookieable > (new QOOKIE(), "assert #32");
+    // would be better with more tests, but I think full stack only.
+    // I don't want to add too much manual-fake code, to test deliverable-code, or I have circular problem about testing
+    // the cookie stuff is quite low cyclometric complexity
+  });
+
+  it("go 2: storeAppearance ", (context) => {
+    const [dom, loc, win] = page("http://192.168.0.35/resource/home", 3);
+    if (!isFullstack(win)) {
+      context.skip();
+    }
+    // if browser look at cookies before and after
+    // look at values in   dom.cookies
+  });
+
+  it("go 3: applyAppearance ", (context) => {
+    const [dom, loc, win] = page("http://192.168.0.35/resource/home", 3);
+    if (!isFullstack(win)) {
+      context.skip();
+    }
+    // if browser look at cookies before and after
+    // look at values in   dom.cookies
+  });
+});
---
+++ src/tests/networking.vitest.ts
@@ -0,0 +1,41 @@
+import { assert, describe, it, assertType } from "vitest";
+
+import { Cookieable, Fetchable } from "../all-types";
+import { TEST_ONLY } from "../networking";
+const { getFetch, runFetch, accessCookie, log, delay } = TEST_ONLY;
+
+describe("TEST networking", () => {
+  it("go 1: getFetch", () => {
+    assert.equal(typeof getFetch, "function", "assert #1");
+    assertType<Fetchable>(getFetch(), "assert #2");
+  });
+
+  it("go 2: accessCookie ", () => {
+    // see new test in webtest unittest
+    assertType<Cookieable>(accessCookie(), "assert #4");
+    // would be better with more tests, but I think full stack only.
+    // I don't want to add too much manual-fake code, to test deliverable-code, or I have circular problem about testing
+    // the cookie stuff is quite low cyclometric complexity
+  });
+
+  it("go 3: runFetch", (context) => {
+    context.skip();
+  });
+
+  it("go 5: delay", async () => {
+    const d1 = new Date();
+    await delay(1000);
+    const d2 = new Date();
+
+    assert.isBelow(d1.getTime() + 1000 - d2.getTime(), 10, "assert #3");
+  });
+
+  it("go 4: log", () => {
+    log("info", "Hello fromn a log test");
+    assert.equal(
+      1,
+      1,
+      "Fake test, as I think that console.log executes.   This code is present, so I could swap to a centralised log platform",
+    );
+  });
+});
