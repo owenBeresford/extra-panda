de0645dc66b3cb8822a43fc9b85181f3af84e2fe
oab1 <oab1@users.sourceforge.net>
2025-03-17 09:34:50 +0000

    Small changes to TS implementation to make building easier

---
+++ vite.config.tests.ts
@@ -0,0 +1,46 @@
+/** @type {import('vite').UserConfig} */
+
+import { defineConfig } from "vite";
+import path, { dirname } from "path";
+import ts from "vite-plugin-ts";
+import { fileURLToPath } from "url";
+// import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
+
+// https://stackoverflow.com/questions/69523560/using-vite-for-backend
+const { builtinModules } = await import('node:module');
+const NODE_BUILT_IN_MODULES = Array.from( builtinModules).filter( m => !m.startsWith('_') );
+NODE_BUILT_IN_MODULES.push(...NODE_BUILT_IN_MODULES.map(m => `node:${m}`));
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+/** @type {import('vite').UserConfig} */
+export default defineConfig({
+  plugins: [ ts() ],
+  root: ".",
+  server: {
+    hmr: false,
+  },
+    optimizeDeps: {
+        exclude: NODE_BUILT_IN_MODULES,
+    },
+  build: {
+    minify: "terser",
+    target: "es2022",
+    lib: {
+      entry: [path.resolve(__dirname, "tools/tests-browser.ts")],
+    },
+    rollupOptions: {
+      plugins: [terser({})],
+      external: NODE_BUILT_IN_MODULES,
+      output: [
+        {
+          format: "es",
+          entryFileNames: "tests-browser.mjs",
+        },
+      ],
+    },
+  },
+});
+
+// vim: syn=javascript nospell
