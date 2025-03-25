fd1f21119309959ce0f172e722faa357809f48d2
oab1 <oab1@users.sourceforge.net>
2025-03-25 16:55:23 +0000

    manual config for uglycss isnt supported

--- tools/compile-css.bash
+++ tools/compile-css.bash
@@ -1,18 +1,18 @@
-#!/bin/bash -x 
+#!/bin/bash 
 PARENT=/var/www/oab1/ast
 
-export compress=" "
-if [ -z "$1" ]; then
+#export compress=" "
+#if [ -z "$1" ]; then
 	echo "INFO: Making production compressed versions of CSS assets"
-	export compress=" --config-file ./.uglyjsrc "
-fi
+#	export compress=" --config-file ./.uglyjsrc "
+#fi
 
 if [ -f /tmp/ob1.css ];then
 	rm /tmp/ob1.css;
 fi
 
-cat $PARENT/fontello.css src/presentation/fonts.css $PARENT/foundation.css ./src/presentation/overloads.css src/presentation/utils.css src/presentation/components.css src/presentation/pages.css $PARENT/jquery-biblio.css src/presentation/colours.css > /tmp/ob1.css
-node14 /usr/bin/uglifycss --debug /tmp/ob1.css > /tmp/ob1.min.css
-mv /tmp/ob1.min.css $PARENT
+cat $PARENT/fontello.css src/presentation/fonts.css $PARENT/foundation.css src/presentation/components.css src/presentation/pages.css src/presentation/utils.css src/presentation/colours.css ./src/presentation/overloads.css > /tmp/ob1.css
+node14 /usr/bin/uglifycss --debug /tmp/ob1.css --output /tmp/ob1.min.css
+cp /tmp/ob1.min.css $PARENT
 
 
