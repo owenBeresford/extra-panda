55803a47eb0977ed656624a1bdf5898bc502df4a
oab1 <oab1@users.sourceforge.net>
2025-04-03 09:45:20 +0100

    Minor adjust and recommpile

--- tools/build-css.bash
+++ tools/build-css.bash
@@ -13,4 +13,5 @@ fi
 cat $PUBLICATION/fontello.css src/presentation/fonts.css $PUBLICATION/foundation.css src/presentation/components.css src/presentation/pages.css src/presentation/utils.css src/presentation/colours.css ./src/presentation/overloads.css > /tmp/ob1.css
 node ./node_modules/.bin/uglifycss --max-line-len 1000 --debug /tmp/ob1.css --output /tmp/ob1.min.css
 cp /tmp/ob1.min.css $PUBLICATION
-
+cp /tmp/ob1.min.css ./src/fixtures/
+ 
