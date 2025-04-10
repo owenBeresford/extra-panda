828a050c2e43607b8b2dd848599bc28e775bc9a7
oab1 <oab1@users.sourceforge.net>
2025-04-10 10:11:41 +0100

    prettier applied

--- src/extractor/types.ts
+++ src/extractor/types.ts
@@ -1,13 +1,11 @@
-
 export type Pseudable = string | null;
 // null values are just not set
 // maybe convert to a TS dictionary  https://stackoverflow.com/questions/15877362/declare-and-initialize-a-dictionary-in-typescript
 // Hashtable: this is a list of CSS attributes
 export type Hashtable = Record<string, string>;
-// iHHT: this is a list of CSS rules       
+// iHHT: this is a list of CSS rules
 export type HashHashtable = Record<string, Hashtable>;
 // HHHT: a CSS block, probably only for a single resolution
 export type HashHashHashtable = Record<string, HashHashtable>;
 
-export type ExportMode= 1 | 2; // 1=CSS, 2=JSON
-
+export type ExportMode = 1 | 2; // 1=CSS, 2=JSON
