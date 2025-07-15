2efdc5ff2fe2874176b11cccbefd33753ab1f579
oab1 <oab1@users.sourceforge.net>
2025-07-15 21:52:19 +0100

    Typo

--- tools/references.bash
+++ tools/references.bash
@@ -1,7 +1,7 @@
 #/bin/bash 
 
 # "node node_modules/.bin/vite-node tools/generate-references2.ts --url http://192.168.1.218/resource/elixir-toolkit  --out /tmp/hfhfhf4.wiki"
-if [ $# -ne 3 ]; then
+if [ $# -eq 3 ]; then
 	echo " run as $0 <url-to-extract> <output-file>"
 	exit 1
 fi
