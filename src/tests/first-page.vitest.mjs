ddecd82f60a7ef668342c6e6bee7b5c5ab6ca2a6
oab1 <oab1@users.sourceforge.net>
2025-07-15 12:34:45 +0100

    Milestone commit, several test files complete

---
+++ src/tests/first-page.vitest.mjs
@@ -0,0 +1,51 @@
+import { assert, describe, it } from "vitest";
+
+import { FirstPage } from "../references/first-page";
+
+// no test for promiseExits, assignClose, 
+describe("TEST references FirstPage ",  () => {
+	it("go 1: FirstPage failure ", async () => {
+		let tmp=new Promise((good, bad) => {
+			const obj=new FirstPage( false);
+			obj.promiseExits(good, bad, -1);
+			obj.failure( new Error("Oh No!") );
+								})
+		.then(
+		 (dat)=>{ assert.equal(1, 0, "K@@@XX0N!!, KL@@@XX0N!! bad, wrong, test die now" ); },
+		 (err)=>{ assert.equal("Error Error: Oh No!", err.toString(), "Error passed through to bad() CB successfully" ); });
+		await Promise.all( [tmp ] );
+	});
+
+	it("go 2: FirstPage success ", async ()=> {
+		let tmp=new Promise((good, bad) => {
+			const obj=new FirstPage( false);
+			obj.promiseExits(good, bad, -1);
+			obj.success("204", "", {});
+								})
+		.then( (dat)=>{ assert.equal([], dat, "parsed empty respoonse without error" ); }, 
+			 (err)=>{ assert.equal(1, 1, "Class reported bad oUtput, as it had no data "+err ); }); 
+
+		let tmp2=new Promise((good, bad) => {
+			const obj=new FirstPage( false);
+			obj.promiseExits(good, bad, -1);
+			let html="<p> <sup><a href=\"sfsdf\">1</a></sup>  <sup><a href=\"sfsdf\">1</a></sup>  <sup><a href=\"sfsdf\">2</a></sup>  <sup><a href=\"sfsdf\">3</a></sup> <sup><a href=\"sfsdf\">4</a></sup>  <sup><a href=\"sfsdf\">5</a></sup>   <sup><a href=\"sfsdf\">6</a></sup>   </p>";
+			obj.success("202", html, {});
+								})
+		.then( (dat)=>{ assert.deepEqual( ["sfsdf", "sfsdf","sfsdf","sfsdf","sfsdf","sfsdf", "sfsdf", ], dat, "parsed fake response without error" ); },
+			 (err)=>{ assert.equal(1, 0, "K@@@XX0N!!, KL@@@XX0N!! bad, wrong, test die now" ); }) ;
+
+		let tmp3=new Promise((good, bad) => {
+			const obj=new FirstPage(false);
+			obj.promiseExits(good, bad, -1);
+			let html="";
+			obj.success("510", html, {});
+								})
+		.then( ( dat)=>{ assert.equal("Recieved 510" , dat.toString(), "Error passed through to bad() CB successfully");}, 
+			 (err)=>{ assert.equal("Error: Recieved 510" , err.toString(), "Error passed through to bad() CB successfully"+err ); });
+		
+		await Promise.all( [tmp, tmp2, tmp3 ] );
+	});
+
+});
+
+
---
+++ src/tests/string-manip.vitest.mjs
@@ -0,0 +1,67 @@
+import { assert, expect, describe, it, vi } from "vitest";
+
+import { TEST_ONLY } from "../references/string-manip";
+const {  publicise_IP, valueOfUrl, normaliseString, baseURL, cleanHTTPstatus } = TEST_ONLY;
+
+describe("TEST references string-manip", () => {
+	it("go 1: publicise_IP ", ()=> {
+		assert.equal("https://google.co.uk/", publicise_IP("https://google.co.uk/"), 'step 1');		
+		assert.equal("https://127.0.0.1/", publicise_IP("https://127.0.0.1/"), 'step 2');		
+		assert.equal("https://8.8.8.8/", publicise_IP("https://8.8.8.8/"), 'step 3');		
+		// NOTE This test has to be HTTP
+		assert.equal("https://owenberesford.me.uk/", publicise_IP("http://192.168.0.0/" ), 'step 4');		
+		assert.equal("https://192.168.1.1/", publicise_IP("https://192.168.1.1/"), 'step 5');		
+	});
+
+	it("go 2: valueOfUrl ", () => {	
+		assert.equal("google.co.uk", valueOfUrl("https://google.co.uk/"), 'step 1');		
+		// not recommended, some-times happens
+		assert.equal("https://google.co.uk", valueOfUrl("https://google.co.uk"), 'step 2');		
+		// stupid option, not really a valid URL
+		assert.equal("https://google.co.uk//", valueOfUrl("https://google.co.uk//"), 'step 3');		
+		// this is for github URLs
+		assert.equal("step2", valueOfUrl("https://google.co.uk/step1/step2/"), 'step 4');		
+		// a full file path URL
+		assert.equal("step2", valueOfUrl("https://google.co.uk/step1/step2/step3.html"), 'step 5');		
+	});
+
+	it("go 3: normaliseString ", () => {
+		assert.equal("TEST", normaliseString("	  TEST "), 'step 1');
+		assert.equal("TEST ".padEnd(500, "#"), normaliseString("	  TEST ".padEnd(10_000, "#") ), 'step 2');	
+		assert.equal("””     ‘‘‘‘  TEST", normaliseString(" &rdquo;&rdquo;&quot;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&lsquo;&lsquo;&lsquo;&lsquo;  TEST "), 'step 3');
+		assert.equal("", normaliseString(""), 'step 4');
+	});
+
+	it("go 4: baseURL ", () => {		
+		assert.equal("https;//blerg.blog", baseURL("https;//blerg.blog#qwerwerw?rgerg=fghf"), 'step 1');
+		assert.equal("https;//blerg.blog", baseURL("https;//blerg.blog"), 'step 2');
+	});
+
+	it("go 4: cleanHTTPstatus ", () => {		
+		assert.equal( 2, cleanHTTPstatus("200"), 'step 1');
+		assert.equal( 2, cleanHTTPstatus("204"), 'step 2');
+		assert.equal( 4, cleanHTTPstatus("404"), 'step 3');
+		assert.equal( 4, cleanHTTPstatus("403"), 'step 4');
+		assert.equal( 5, cleanHTTPstatus("512"), 'step 5');
+	});
+
+/**
+	it("go 1.1: publicise_IP ", ()=> {	
+//		expect( ()=> publicise_IP("http://192.168.0.0/") ).toThrow(new Error(''))
+		const spy = vi.spyOn(console, 'log').mockImplementation(() => {})
+		publicise_IP("http://192.168.0.0/");
+		expect(spy).toHaveBeenCalled();
+		spy.mockRestore();
+	});
+
+	it("go 1.2: publicise_IP ", ()=> {	
+//		expect( ()=>publicise_IP("http://192.168.0.0/") ).toThrow(new Error(''))
+		const spy = vi.spyOn(console, 'log').mockImplementation(() => {})
+		publicise_IP("https://192.168.0.0/");
+		expect(spy).not.toHaveBeenCalled();
+		spy.mockRestore();
+	});
+*/
+});
+
+
