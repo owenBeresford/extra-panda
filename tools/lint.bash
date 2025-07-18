0f62f60df4825a6f60972763f7d7541d7cdc7c03
oab1 <oab1@users.sourceforge.net>
2025-07-18 07:24:12 +0100

    Make lint not report on bash files

--- tools/lint.bash
+++ tools/lint.bash
@@ -2,8 +2,8 @@
 
 node ./node_modules/.bin/eslint --exit-on-fatal-error --no-cache ./src/*.ts;  
 node ./node_modules/.bin/eslint --exit-on-fatal-error --no-cache src/tests/*; 
-node ./node_modules/.bin/eslint --exit-on-fatal-error --no-cache tools/*; 
+node ./node_modules/.bin/eslint --exit-on-fatal-error --no-cache tools/*.ts tools/*mjs; 
 node ./node_modules/.bin/prettier --write ./src; # this will do sub-dirs 
-node ./node_modules/.bin/prettier --write ./tools/; 
+node ./node_modules/.bin/prettier --write ./tools/*.ts ./tools/*.mjs; 
 git checkout src/fixtures/*mjs; 
 
