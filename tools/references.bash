f7f2673989044d2568fced6d0a89f8c5b3622165
oab1 <oab1@users.sourceforge.net>
2025-07-18 07:22:39 +0100

    Extend this wrapper, mostly error messages

--- tools/references.bash
+++ tools/references.bash
@@ -1,12 +1,17 @@
 #/bin/bash 
+# UPDATE: look up this ip
+myip=192.168.1.218
 
-# "node node_modules/.bin/vite-node tools/generate-references2.ts --url http://192.168.1.218/resource/elixir-toolkit  --out /tmp/hfhfhf4.wiki"
-if [ $# -eq 3 ]; then
-	echo " run as $0 <url-to-extract> <output-file>"
+if [ $# -eq 1 ]; then
+	echo " filename to URL conversion not written, please add it if needed"
+	exit 2
+elif [ $# -ne 2 ]; then
+	echo " Run as $0 <url-to-extract> <output-file>"
 	exit 1
 fi
 
-node ./node_modules/.bin/vite-node tools/generate-references2.ts --url $1 --out $2
+# "node node_modules/.bin/vite-node tools/generate-references2.ts --url http://192.168.1.218/resource/elixir-toolkit  --out /tmp/hfhfhf4.wiki"
+node ./node_modules/.bin/vite-node tools/generate-references2.ts --url http://$myip/resource/$1 --out $2
  
 
 
