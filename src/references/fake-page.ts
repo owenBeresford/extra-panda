426aec0ace9438ddff9f410d972ff3ced273bd1d
oab1 <oab1@users.sourceforge.net>
2025-07-23 07:47:29 +0100

    Migrate from OO setter so can bind(); Improve logic on compound error states

--- src/references/fake-page.ts
+++ src/references/fake-page.ts
@@ -17,45 +17,48 @@ export class FakePage implements HTMLTransformable {
     this.assignClose = this.assignClose.bind(this);
     this.success = this.success.bind(this);
     this.failure = this.failure.bind(this);
+    this.setState = this.setState.bind(this);
 
     this.CB = false;
     this._state = 0;
   }
 
   // the "100 category" for HTTP codes, i.e. 2, 4, 5
-  public set state(no: number) {
+  public setState(no: number) {
     console.log("Running setState with " + no);
     this._state = no;
   }
 
   public success(statusCode: string, data: string): void {
     // also param headers:Headers
-    console.log("WWWW ", statusCode, cleanHTTPstatus(statusCode), this._state);
+    console.log("WWWW ", {"raw":statusCode, "cooked":cleanHTTPstatus(statusCode), "target":this._state});
     assert.equal(
       this._state,
       cleanHTTPstatus(statusCode),
       "Server returned desired results " + statusCode,
     );
     if (typeof this.CB === "function") {
-      console.log("Running 1st cURL close");
+      console.log("success(): Running cURL close");
       this.CB();
     }
 
     if (cleanHTTPstatus(statusCode) !== this._state) {
-      this.bad(new Error("Recieved " + statusCode));
+      this.bad(new Error("Recieved " + statusCode) );
     } else {
       this.good([]);
     }
   }
 
   public failure(msg: Error | string): void {
-    console.warn(msg);
+    console.warn(msg, "and ", this._state);
 
     if (typeof this.CB === "function") {
+      console.log("failure(): Running cURL close");
       this.CB();
     }
     assert.equal(this._state, 5, "Server returned desired results " + msg);
-    this.bad("Error " + msg);
+	if(this._state === 5) { this.good([]); }
+    else { this.bad("Error: " + msg); }
   }
 
   public promiseExits(good: PromiseCB, bad: PromiseCB, offset: number): void {
