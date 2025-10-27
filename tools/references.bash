9a19a0b9df5adfafaf0e6699de2b111d17e7a0a1
oab1 <oab1@users.sourceforge.net>
2025-10-27 09:54:53 +0000

    Improve script, mostly comments added

--- tools/references.bash
+++ tools/references.bash
@@ -1,5 +1,7 @@
 #/bin/bash 
-# UPDATE: look up this ip
+# #TODO: look up this IP
+# To build a patch on an existing file append --patch to the command
+# Note, running vite-node not node, which is why this script,
 myip=192.168.1.218
 
 if [ $# -eq 1 ]; then
@@ -10,8 +12,8 @@ elif [ $# -ne 2 ]; then
 	exit 1
 fi
 
-# "node node_modules/.bin/vite-node tools/generate-references2.ts --url http://192.168.1.218/resource/elixir-toolkit  --out /tmp/hfhfhf4.wiki"
-node ./node_modules/.bin/vite-node tools/generate-references2.ts --url http://$myip/resource/$1 --out $2
+# SAMPLE: "node node_modules/.bin/vite-node tools/generate-references2.ts --url http://192.168.1.218/resource/elixir-toolkit  --out /tmp/hfhfhf4.wiki"
+node ./node_modules/.bin/vite-node tools/generate-references2.ts --url http://$myip/resource/$1 --out $2 
  
 
 
