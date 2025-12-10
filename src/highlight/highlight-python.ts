a8bc8e7fc3678543d333f8afbd6c05f199f76b74
oab1 <oab1@users.sourceforge.net>
2025-12-10 13:20:22 +0000

    Add python as a syntax highlight option

---
+++ src/highlight/highlight-python.ts
@@ -0,0 +1,26 @@
+import hljs from "highlight.js/lib/core";
+import { Language, HLJSApi } from "highlight.js";
+import * as javascript from "highlight.js/lib/languages/python";
+
+// custom written loader as code is packed in a legacy fashion
+hljs.registerLanguage("python", (hl: HLJSApi): Language => {
+  return javascript.default(hl);
+});
+
+/**
+ * execHighlight
+ * Apply the highlighting for the current language
+ 
+ * @param {Document = document} dom
+ * @public
+ * @returns {void}
+ */
+export function execHighlight(dom: Document = document): void {
+  dom.querySelectorAll('code[lang="python"], code[lang="py3"]').forEach((el: HTMLElement): void => {
+    hljs.highlightElement(el);
+  });
+}
+
+if (typeof window.process === "undefined") {
+  execHighlight(document);
+}
