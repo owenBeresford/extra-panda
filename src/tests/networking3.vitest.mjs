558a88236fb95611fdc3e7bad676859d5ae09e03
oab1 <oab1@users.sourceforge.net>
2025-12-30 07:06:56 +0000

    I pulled this test out, as it needs cURL loaded directly.

---
+++ src/tests/networking3.vitest.mjs
@@ -0,0 +1,42 @@
+// the asserts are in the worker class
+import { describe, it, vi } from "vitest";
+import { networkInterfaces } from "node:os";
+import { Curl } from "node-libcurl";
+import { TEST_MACHINE } from "../immutables";
+
+import {
+  fetch2,
+  exec_reference_url,
+  delay,
+  mapInterfaces,
+	TEST_ONLY,
+} from "../references/networking";
+
+vi.setConfig({ testTimeout: 0 });
+
+describe("TEST references networking2 ", () => {
+  const LAN_IP =
+    "http://" + mapInterfaces(networkInterfaces())["first"][0] + "/";
+  const BAD_IP = TEST_MACHINE;
+
+	it.sequential("go 4: urlFiltering ",  () => {
+  		const curl = new Curl();
+		const { urlFiltering } = TEST_ONLY;
+		// This is the way https://manpages.debian.org/bullseye-backports/libcurl4-doc/curl_url_get.3.en.html
+
+		let url = "http://192.168.1.218/";
+		assert.equal( urlFiltering(url, curl), curl, "assert #1");
+// console.log("WERWER {{", curl, "}}" );
+
+		url = "https://stackoverflow.com/gdgdfg/ddgdfgkdfgsfs?fhfhf=fdgfhfgh&dfgdfgrer";
+		assert.equal( urlFiltering(url, curl), curl, "assert #2");
+// console.log("WERWER2 {{", curl, "}}" );
+
+		url = "https://unicode.org/gdgdfg/ddgdfgkdfgsfs?fhfhf=fdgfhfgh&dfgdfgrer";
+		assert.equal( urlFiltering(url, curl), curl, "assert #3");
+// console.log("WERWER3 {{", curl, "}}" );
+
+	});
+
+});
+
