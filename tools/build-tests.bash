90b7c782c4cc1d6cfdfe3177534ec713216b5bd8
oab1 <oab1@users.sourceforge.net>
2026-08-25 09:30:27 +0100

    Simplify build script

--- tools/build-tests.bash
+++ tools/build-tests.bash
@@ -9,8 +9,6 @@ done
 
 rm ./dist/tests/*.js; 
 node ./node_modules/.bin/vite -c vite.config.index.webtest.ts build;
-export FN=`ls ./dist/index.webtest* | sed -e 's/\.\/dist\///' `
-sed -i ./dist/index-* -e 's/'$FN'.*/index\.webtest\.mjs";/'
 mv ./dist/index.webtest* ./dist/tests/index.webtest.mjs
 mv ./dist/index-* ./dist/tests/
 echo -e '\n\n\t\tAlways add more tests' 
